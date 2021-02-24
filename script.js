9bd7020523b935f7420a063d484d2d6e7b2275db
waleedbaroudi <waleedbaroudi@hotmail.com>
2021-02-25 01:12:23 +0300

    make intro flag mobile compatible

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var topButton = document.getElementById("top-button");
+let topButton = document.getElementById("top-button");
+let introFlag = document.getElementById("intro-flag");
+let initFlagMargin = introFlag.style.marginLeft;
+let introFlagArrow = document.getElementById("arrow");
+
+let flagOpen = false;
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
@@ -11,6 +16,23 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
     let scrolled =  document.body.scrollTop > 50 || document.documentElement.scrollTop > 50;
     topButton.style.display = (scrolled) ? "block" : "none";
+}
+
+function expandFlag() {
+    if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        return;
+    }
+    
+    if(flagOpen){
+        introFlag.style.marginLeft = initFlagMargin;
+        introFlagArrow.style.transform = "rotate(0deg)";
+    }
+    else{
+        introFlag.style.marginLeft = 0;
+        introFlagArrow.style.transform = "rotate(180deg)";
+    }
+    
+    flagOpen = !flagOpen;
 }
 
 function toTop(){
